79d01bdb27d3768513fa4e79c863905995820d5a
Marcel Balint <blank_blankness@yahoo.com>
2022-09-11 10:23:27 -0400

    Added the html

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+import {v4 as uuidV4} from 'uuid';
+console.log(uuidV4)
+
+const list = document.querySelector('#task');
+const form = document.querySelector('#new-task-from');
+const input = document.querySelector('#new-task-title');
+
