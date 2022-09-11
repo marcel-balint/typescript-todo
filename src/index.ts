882ed95313337a37668413456f2477c2d5fb4975
Marcel Balint <blank_blankness@yahoo.com>
2022-09-11 11:18:24 -0400

    Added functionality to add a task

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 
-import {v4 as uuidV4} from 'uuid';
+import {v4 as uuidV4} from 'uuid'
 
 
 type Task = {
@@ -9,27 +9,55 @@ type Task = {
     createdAt: Date
 }
 
-const list = document.querySelector<HTMLUListElement>('#task');
-const form = document.getElementById('new-task-from') as HTMLFormElement | null;
-const input = document.querySelector<HTMLInputElement>('#new-task-title');
+const list = document.querySelector<HTMLUListElement>('#list')
+const form = document.getElementById('new-task-from') as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>('#new-task-title')
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
 
 form?.addEventListener('submit', e => {
-    e.preventDefault();
+    e.preventDefault()
+ 
 
     if(input?.value == '' || input?.value == null) return
 
     const newTask: Task = {
         id: uuidV4(),
         title: input.value,
-        completed: true,
+        completed: false,
         createdAt: new Date()
     }
+    tasks.push(newTask)
+    saveTasks()
+    addListItem(newTask)
 
-addListItem(newTask);
-
-
+     input.value = ''
 })
 
-function addListItem (task: Task){
+function addListItem(task: Task){
+   const item = document.createElement('li')
+   const label = document.createElement('label')
+   const checkbox = document.createElement('input')
+   checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+   checkbox.type = 'checkbox'
 
+   checkbox.checked = task.completed
+   label.append(checkbox, task.title)
+   item.append(label)
+   list?.append(item)
 }
+
+function saveTasks() {
+    localStorage.setItem("TASKS", JSON.stringify(tasks))
+  }
+
+  function loadTasks(): Task[] {
+    const taskJSON = localStorage.getItem("TASKS")
+    if (taskJSON == null) return []
+    return JSON.parse(taskJSON)
+  }
+
+  
